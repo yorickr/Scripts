8a7b31f695379416647b77f20e6fc22ee646f79b
Yorick Rommers <yorick-rommers@hotmail.com>
2015-06-11 19:08:56 +0200

    mpd

--- mp.sh
+++ mp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sh ~/Scripts/mplayerthing.sh & ncmpcpp ; $(ps aux | grep "sh /home" | grep -v grep | awk '{print "killall mplayer && kill " $2}' | sh)
+ssh -p 777 -N -L9999:127.0.0.1:9999 imegumii@www.imegumii.nl & ssh -p 777 -N -L6600:127.0.0.1:6600 imegumii@www.imegumii.nl & sh ~/Scripts/mplayerthing.sh & ncmpcpp ; $(ps aux | grep "sh /home" | grep -v grep | awk '{print "killall mplayer && killall ssh && kill " $2}' | sh)
 
--- mplayerthing.sh
+++ mplayerthing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 runplayer=true
 while $runplayer
-do nohup mplayer "http://internal.imegumii.nl:9999/mpd.mp3" > /dev/null
+do nohup mplayer "http://localhost:9999/mpd.mp3" > /dev/null
   sleep 3
 done 
